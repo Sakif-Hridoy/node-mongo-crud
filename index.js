f2d3c584629630b015759c17eefb882934202ef8
Sakif Hridoy <sakifworkspace@gmail.com>
2021-12-10 19:21:14 +0600

    connection script commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+// const MongoClient = require('mongodb').MongoClient;
+const password = "pQEM60MP1iIqxa67";
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://organicUser:pQEM60MP1iIqxa67@cluster0.djg6r.mongodb.net/organicdb?retryWrites=true&w=majority";
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("hello I am working")
+})
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
+
+app.listen(3000);
