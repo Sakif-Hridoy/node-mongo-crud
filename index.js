a718f585f40b5fee38f7c9ae93e107d4831f95a8
Sakif Hridoy <sakifworkspace@gmail.com>
2021-12-11 16:39:09 +0600

    GET/READ Method commit

--- index.js
+++ index.js
@@ -9,19 +9,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({exxtended: false}));
 
+// ROOT
 app.get('/',(req,res)=>{
     res.sendFile(__dirname +"/index.html")
-    
-    
 })
 
 //  client side database connection function script with INSERT in a directory
 client.connect(err => {
   const productCollection = client.db("organicdb").collection("products");
-  app.post("/addProduct",(req,res)=>{
+
+
+// INSERT Directory/Route
+app.post("/addProduct",(req,res)=>{
     //   POST/INSERT METHOD
     // Here data is creating from index.html and then "req"(requesting)to its body to get data then responding from index.js as "success"
     const product = req.body;
+    //   POST/INSERT METHOD
     productCollection.insertOne(product)
     .then(result=>{
         console.log('data added successfully')
@@ -29,6 +32,19 @@ client.connect(err => {
 
     })
   })
+
+// READ Directory/Route
+app.get('/products',(req,res)=>{
+    // must read mongo crud docs to know this ind of features 
+    /**/
+    // productCollection.find({}).limit(5)
+    /**/
+    // GET/READ Method
+    productCollection.find({})
+    .toArray((err,result)=>{
+        res.send(result);
+    })
+})
   
   console.log("database connected")
   // perform actions on the collection object
